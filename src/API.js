da28023f396049f972765121f156f8bf8cd85e33
Ryan Riley <ryan.b.riley@colorado.edu>
2017-08-19 10:14:03 -0600

    adding rewards request

--- src/API.js
+++ src/API.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 const API_URL = 'https://api.devexhacks.com';
 const POST_URL = 'https://api.devexhacks.com';
+const REWARDS_URL = 'https://api.devexhacks.com/rewards/account/';
 
 export const getCapOne = () => {
     return axios.get(API_URL)
   }
 export const addCapOne = (data) => {
   return axios.post(POST_URL, data)
+}
+export const getRewards = (user) => {
+  return axios.get(REWARDS_URL, user)
 }
