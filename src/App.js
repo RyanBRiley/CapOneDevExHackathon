2435787a9ad68ab4215106e94c06837b01f91c57
Jason Clifton <j.clifton1473@gmail.com>
2017-08-19 11:00:06 -0600

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import Rewards from './components/Rewards';
+import Home from './components/Home';
+
+import { Input, Menu, Segment } from 'semantic-ui-react'
 
 class App extends Component {
+  state = { activeItem: 'Home' }
+
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+  }
+
   render() {
+    const { activeItem } = this.state
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <BrowserRouter>
+        <Switch>
+        <Menu pointing>
+          <Menu.Item as={Link} to='/Home' name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick} />
+          <Menu.Item as={Link} to='/Rewards' name='Rewards' active={activeItem === 'Rewards'} onClick={this.handleItemClick} />
+        </Menu>
+        <Route
+          path='/Home'
+          component={Home}
+         />
+
+          <Route
+            path='/Rewards'
+            render = {(props) => (
+             <Rewards {...props}
+             />
+            )}
+          />
+        </Switch>
+        </BrowserRouter>
+        {this.state.activeItem === 'Home' &&
+          <Home />
+        }
+        {this.state.activeItem === 'Rewards' &&
+          <Rewards />
+        }
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/Rewards.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Rewards extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div>
+      {console.log('In the Rewards')}
+      <p>rewards</p>
+      </div>
+    )
+  }
+}
+export default Rewards;
