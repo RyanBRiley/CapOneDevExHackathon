2cf5464e650fe30e5f7c0edfb4e2d5d0a6a82b5b
Ryan Riley <ryan.b.riley@colorado.edu>
2017-08-19 10:35:53 -0600

    fixing API requests

--- src/API.js
+++ src/API.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+const AUTH = 'Bearer eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwicGNrIjoxLCJhbGciOiJkaXIiLCJ0diI6Miwia2lkIjoiYTdxIn0..Q8EPUTo189PyagVaeXKw9XgvYN1pEz5Vgp1bgF4Hj9TE2anFkmGILcf7UX9iO6L0cUTgJQm3blatkUZUyUKc6cHFyyuVPKmtZDIU2zmP6VEhxmroUfeqh8YJnOEw9LRVKU1Pq4fVRuZMsIM1Mf6F2oMOAFL8JTw7AK4CQVUWtti4KHaNBtDX9cHOuwRtDbKhQbmySLP0g5ENzrC9gWMLprmq66hX5bI4TAiF2f7KlgjtT9lvph9pLyDsfBhtOanWj6gVmYMqxcNQlUHcgtsH3nlthX1PsOKQppDtmS09hPELzTxEn2kxk2btJ0KPy2iQFQyDSWfER1xgJnFDASr1sg8MNeQh3Qjmp4vuruQMimu1IFVvb1cIsIDS7cWPCUPa2UFYz9YfW1uXVnUpOyZTCWZ3E28YL70Rn2TbP4Hw030rgBWF5Ok1YD51e7BWJXXCq1lIWUG85WmjWZ5Il4nVNZBxBFDPR7lQMG2Gw36ibffzfTDwwHfWhlpkmbqtRLawKEVtYNDcpIvocujQJFHlwCRJ9uex5BXJzQQ6Mrp1cvxp3sp65mU5EPSU4J1OK0Iuj8Yv.I3YRuEIDtnqtHjjjrb9OK0A';
 const API_URL = 'https://api.devexhacks.com';
 const POST_URL = 'https://api.devexhacks.com';
 const REWARDS_URL = 'https://api.devexhacks.com/rewards/account/';
@@ -9,11 +10,11 @@ export const getCapOne = () => {
 export const addCapOne = (data) => {
   return axios.post(POST_URL, data)
 }
-export const getRewards = (user) => {
-  var rewards_path = REWARDS_URL+user
-  return axios.get(rewards_path)
+export const getRewards = () => {
+  var rewards_request = '${REWARDS_URL}?Authorization=${AUTH}';
+  return axios.get(rewards_request)
 }
 export const getAcctDetail = (acct_id) => {
-  var acct_path = REWARDS_URL+acct_id;
-  return axios.get(acct_path)
+  var acct_request = '${REWARDS_URL}${acct_id}?Authorization=${AUTH}?'
+  return axios.get(acct_request)
 }
