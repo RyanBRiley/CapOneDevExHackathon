441485c8d1b1f4cf078ebfebfcaef93527010c0a
Ryan Riley <ryan.b.riley@colorado.edu>
2017-08-19 17:30:34 -0600

    adding lipstick

--- src/App.js
+++ src/App.js
@@ -5,10 +5,30 @@ import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import Rewards from './components/Rewards';
 import Home from './components/Home';
 import { getCapOne } from './API';
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Visibility,
+} from 'semantic-ui-react'
 
-import { Input, Menu, Segment } from 'semantic-ui-react'
+const FixedMenu = () => (
+  <Menu fixed='top' size='large'>
+    <Container>
+      <Menu.Item as='a' active>Home</Menu.Item>
+      <Menu.Item as='a'>Rewards</Menu.Item>
+    </Container>
+  </Menu>
+) 
 
-class App extends Component {
+export default class HomepageLayout extends Component {
   constructor(){
     super()
     this.state={
@@ -16,9 +36,11 @@ class App extends Component {
       rewards: []
     }
   }
-
-  handleItemClick = (e, { name }) => {
+  // state = {}
+  handleItemClick = (e, { name }) =>{
+    console.log('name: ' + name)
     this.setState({ activeItem: name })
+    console.log('State is :' + this.state)
   }
   componentDidMount(){
     getCapOne().then(result => {
@@ -27,32 +49,55 @@ class App extends Component {
     })
   }
 
+  hideFixedMenu = () => this.setState({ visible: false })
+  showFixedMenu = () => this.setState({ visible: true })
+
   render() {
-    const { activeItem } = this.state
+    const { visible, activeItem } = this.state
 
     return (
       <div>
-        <BrowserRouter>
-        <Switch>
-        <Menu pointing>
-          <Menu.Item as={Link} to='/Home' name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick} />
-          <Menu.Item as={Link} to='/Rewards' name='Rewards' active={activeItem === 'Rewards'} onClick={this.handleItemClick} />
-        </Menu>
-        <Route
-          path='/Home'
-          component={Home}
-         />
+        { visible ? <FixedMenu /> : null }
 
-          <Route
-            path='/Rewards'
-            render = {(props) => (
-             <Rewards {...props}
-             />
-            )}
-          />
-        </Switch>
-        </BrowserRouter>
-        {this.state.activeItem === 'Home' &&
+        <Visibility
+          onBottomPassed={this.showFixedMenu}
+          onBottomVisible={this.hideFixedMenu}
+          once={false}
+        >
+          <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 50, padding: '1em 0em'}}
+            vertical
+          >
+            <Container>
+            <BrowserRouter>
+            <Switch>
+              <Menu inverted pointing>
+                <Menu.Item as={Link} to='/Home' name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick}>Home</Menu.Item>
+                <Menu.Item as={Link} to='/Rewards' name='Rewards' active={activeItem === 'Rewards'} onClick={this.handleItemClick}>Rewards</Menu.Item>
+              </Menu>
+              <Route
+                path='/Home'
+                component={Home}
+                />
+                <Route
+                path='/Rewards'
+                render = {(props) => (
+                  <Rewards {...props}
+                  />
+                  )}
+                />
+                </Switch>
+                </BrowserRouter>
+            </Container>
+            </Segment>
+            <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 350, padding: '1em 0em', background: 'blue' }}
+            vertical>
+            {this.state.activeItem === 'Home' &&
           <Home />
         }
         {this.state.activeItem === 'Rewards' &&
@@ -60,9 +105,46 @@ class App extends Component {
             rewards={this.state.rewards}
           />
         }
+            
+            </Segment>
+            <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 350, padding: '1em 0em', background: 'white' }}
+            vertical>
+            <Container>
+            <img src='https://timedotcom.files.wordpress.com/2015/10/bestbanks_capitalone.png?w=814' width="400"/>
+            </Container>
+            
+          </Segment>
+        </Visibility>
+
+
+        <Segment inverted vertical style={{ padding: '5em 0em' }}>
+          <Container>
+            <Grid divided inverted stackable>
+              <Grid.Row>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='About' />
+                  <List link inverted>
+                    <List.Item as='a'>Sitemap</List.Item>
+                    <List.Item as='a'>Contact Us</List.Item>
+                    <List.Item as='a'>Capital One</List.Item>
+                    <List.Item as='a'>DevExchange</List.Item>
+                  </List>
+                </Grid.Column>
+               
+                <Grid.Column width={7}>
+                  <Header as='h4' inverted>This is a work in progress</Header>
+                  <p>We are new to this and further development is needed.</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
       </div>
     )
   }
 }
 
-export default App;
+// export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Visibility,
+} from 'semantic-ui-react'
 
 class Home extends Component {
   constructor() {
     super()
   }
+  handleItemClick = (e, { name }) =>{
+    console.log('name: ' + name)
+    this.setState({ activeItem: name })
+    console.log('State is :' + this.state)
+  }
   render() {
     return (
       <center><div>
-        <p>Capital One Rewards++</p>
-         <img src='https://timedotcom.files.wordpress.com/2015/10/bestbanks_capitalone.png?w=814' width="400"/>
+        <Container>
+
+              <Header
+                as='h1'
+                content='Rewards++'
+                inverted
+                style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '1em', color:'white'}}
+              />
+              <Header
+                as='h2'
+                content='Get the most out of your Capital One rewards'
+                inverted
+                style={{ fontSize: '1.7em', fontWeight: 'normal', color: 'white'}}
+              />
+              <Button name='Rewards' primary size='huge' onClick={this.handleItemClick}>
+                View your rewards
+                <Icon name='right arrow' />
+              </Button>
+            </Container>
       </div></center>
     )
   }
